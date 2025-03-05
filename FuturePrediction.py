0ff4d1fad60f57af09376203bce82dc3364d7b01
onurhanakbulut <onurhanakbulut1@hotmail.com>
2025-03-06 00:51:13 +0300

    ML Algorithms3

---
+++ FuturePrediction.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+#-------------MY TEST DATA----------------
+df = pd.read_csv('futuredata1.csv')
+mytest = df.iloc[:,1:].values
+
+#----------------JOBLIB LOAD--------------------
+pr_pipeline = joblib.load('poly_pipeline.pkl')
+
+
+
+
+#----------------POLYNOMIAL PREDICTION (0.8423)-----------
+predict_pr = pr_pipeline.predict(mytest)
+
+
+
+
+
+
+
+
+
+#------------------CSV---------------------------------
+predict_pr = pd.DataFrame(predict_pr)
+
+prediction = pd.concat([df,predict_pr], ignore_index=True, axis=1)
+
+prediction = prediction.drop(columns=[1])
+
+
+prediction.columns = ['Country', 'Year', 'Month', 'Average Surface Temperature']
+
+prediction.to_csv('FuturePrediction.csv', index=False)
--- test.py
+++ test.py
@@ -5,10 +5,7 @@ import joblib
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 
-df = pd.read_csv('futuredata1.csv')
-mytest = df.iloc[:,1:].values
-
-
+#----------------JOBLIB LOAD--------------------
 lr_model = joblib.load('lr_model.pkl')
 pr_pipeline = joblib.load('poly_pipeline.pkl')
 svr_pipeline = joblib.load('svr_model.pkl')
@@ -16,22 +13,96 @@ dt_model = joblib.load('dt_model.pkl')
 rf_model = joblib.load('rf_model.pkl')
 
 
-
+#-------------------JOBLIB TEST LOAD------------------------
 x_test = joblib.load("x_test.pkl")
 y_test = joblib.load("y_test.pkl")
 
+
+
+#------------------LINEAR REGRESSION EVALUATE------------------------
 predict_lr = lr_model.predict(x_test)
 
 
 
-# Performans değerlendirme
+
 r2 = r2_score(y_test, predict_lr)
 mae = mean_absolute_error(y_test, predict_lr)
 mse = mean_squared_error(y_test, predict_lr)
 rmse = np.sqrt(mse)
 
-# Sonuçları yazdır
+
 print("📊 Linear Regression Model Performansı:")
+print(f"R² Skoru: {r2:.4f}")
+print(f"MAE: {mae:.4f}")
+print(f"MSE: {mse:.4f}")
+print(f"RMSE: {rmse:.4f}")
+
+
+#---------------------POLYNOMIAL REGRESSON EVALUATE-----------------
+predict_pr = pr_pipeline.predict(x_test)
+
+
+
+r2 = r2_score(y_test, predict_pr)
+mae = mean_absolute_error(y_test, predict_pr)
+mse = mean_squared_error(y_test, predict_pr)
+rmse = np.sqrt(mse)
+
+
+print("📊 Polynomial Regression Model Performansı:")
+print(f"R² Skoru: {r2:.4f}")
+print(f"MAE: {mae:.4f}")
+print(f"MSE: {mse:.4f}")
+print(f"RMSE: {rmse:.4f}")
+
+
+#------------------SVR EVALUEATE---------------------
+
+
+predict_svr = svr_pipeline.predict(x_test)
+
+
+
+
+r2 = r2_score(y_test, predict_svr)
+mae = mean_absolute_error(y_test, predict_svr)
+mse = mean_squared_error(y_test, predict_svr)
+rmse = np.sqrt(mse)
+
+
+print("📊 SVR Model Performansı:")
+print(f"R² Skoru: {r2:.4f}")
+print(f"MAE: {mae:.4f}")
+print(f"MSE: {mse:.4f}")
+print(f"RMSE: {rmse:.4f}")
+
+#--------------------DECISION TREE EVALUEATE-----------------------
+
+predict_dt = dt_model.predict(x_test)
+
+r2 = r2_score(y_test, predict_dt)
+mae = mean_absolute_error(y_test, predict_dt)
+mse = mean_squared_error(y_test, predict_dt)
+rmse = np.sqrt(mse)
+
+
+print("📊 DT Model Performansı:")
+print(f"R² Skoru: {r2:.4f}")
+print(f"MAE: {mae:.4f}")
+print(f"MSE: {mse:.4f}")
+print(f"RMSE: {rmse:.4f}")
+
+#------------------------RANDOM FOREST EVALUATE-----------------------
+predict_rf = rf_model.predict(x_test)
+
+
+r2 = r2_score(y_test, predict_rf)
+mae = mean_absolute_error(y_test, predict_rf)
+mse = mean_squared_error(y_test, predict_rf)
+rmse = np.sqrt(mse)
+
+
+print("📊 RF Model Performansı:")
 print(f"R² Skoru: {r2:.4f}")
 print(f"MAE: {mae:.4f}")
 print(f"MSE: {mse:.4f}")
@@ -44,24 +115,6 @@ print(f"RMSE: {rmse:.4f}")
 
 
 
-
-
-
-
-
-# from sklearn.preprocessing import StandardScaler
-# sc = StandardScaler()
-# scaled_test = sc.fit_transform(test)
-
-
-
-
-
-# predict_lr = lr_model.predict(test)
-# predict_pr = pr_pipeline.predict(test)
-# predict_svr = svr_pipeline.predict(scaled_test)
-# predict_dt = dt_model.predict(test)
-# predict_rf = rf_model.predict(test)
 
 
 
