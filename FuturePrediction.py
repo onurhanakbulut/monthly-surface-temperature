7818ac8fb5057e7c79bb0704fb18b03bde906b02
onurhanakbulut <onurhanakbulut1@hotmail.com>
2025-03-06 01:50:39 +0300

    Obtained Data

--- FuturePrediction.py
+++ FuturePrediction.py
@@ -21,10 +21,6 @@ predict_pr = pr_pipeline.predict(mytest)
 
 
 
-
-
-
-
 #------------------CSV---------------------------------
 predict_pr = pd.DataFrame(predict_pr)
 
@@ -36,3 +32,30 @@ prediction = prediction.drop(columns=[1])
 prediction.columns = ['Country', 'Year', 'Month', 'Average Surface Temperature']
 
 prediction.to_csv('FuturePrediction.csv', index=False)
+
+
+
+
+#------------------------analyze visualization------------------
+from plotly import express
+
+express.scatter(prediction, x='Month', y='Average Surface Temperature', trendline='lowess').show(renderer='iframe_connected')
+
+
+
+
+
+
+# #------------------------analyze visualization------------------
+# from plotly import express
+
+# express.scatter(data_frame=df[df['Entity'] == 'World'].drop(columns=['Code', 'Day', 'Average surface temperature']).drop_duplicates()
+# , x='year', y='Average surface temperature.1', trendline='lowess').show(renderer='iframe_connected',)
+
+
+
+
+
+
+
+
